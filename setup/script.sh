0f148a0599da35adb224e75467c947628a17be5e
Osama Oransa <osa_ora@yahoo.com>
2025-01-19 19:19:38 +0400

    Update script.sh

--- setup/script.sh
+++ setup/script.sh
@@ -25,7 +25,7 @@ echo "MySQL Pod name $POD_NAME"
 
 # Install the DB schema and add some data
 # Display Schema and data installing sql statements
-echo "Will install: $COMMAND
+echo "Will install: $COMMAND"
 oc exec $POD_NAME -- mysql -u root accountdb -e "create table accountdb.account ( id INT AUTO_INCREMENT NOT NULL PRIMARY KEY, firstname VARCHAR( 255 ) NOT NULL, lastname VARCHAR( 255 ) NOT NULL,status INT NOT NULL);insert into accountdb.account (id, firstname, lastname, status) values (1,'Osama','Oransa',1);insert into accountdb.account (id, firstname, lastname, status) values (2,'Osa','Ora',1);"  
 # Check DB content
 oc exec $POD_NAME -- mysql -u root accountdb -e "select * from accountdb.account" 
